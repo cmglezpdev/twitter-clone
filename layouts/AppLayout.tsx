b654457d5babbb5d841b6e96f0d6afacbe9aa7b2
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-25 22:05:43 -0500

    chore: start app

---
+++ layouts/AppLayout.tsx
@@ -0,0 +1,10 @@
+
+const AppLayout = () => {
+    return (
+        <h1 className="text-3xl font-bold underline text-red-200">
+          Hello world!
+        </h1>
+    )
+}
+
+export default AppLayout
