54a5e21f00418a589022d31af18e8f3af2affd81
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-26 20:42:00 -0500

    feat: add tweet model

---
+++ models/Tweet.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, model, Model } from 'mongoose';
+import { ITweet } from '../interfaces';
+
+const tweetSchema = new Schema({
+    text: { type: String, required: true },
+    user: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
+    views: { type: [mongoose.Types.ObjectId], ref: 'User' },
+    likes: { type: [mongoose.Types.ObjectId], ref: 'User' },
+    retweets: { type: [mongoose.Types.ObjectId], ref: 'User' },
+    comments: { type: [mongoose.Types.ObjectId], ref: 'Tweet' },
+}, {
+    timestamps: true
+})
+
+const Tweet:Model<ITweet> =  mongoose.models.Tweet || model('Tweet', tweetSchema);
+
+export default Tweet;
