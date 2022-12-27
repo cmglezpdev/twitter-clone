d04717a6d42549841bbde0390462cff9e348707b
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-27 14:29:08 -0500

    feat: add useForm Hook

---
+++ hooks/useForm.ts
@@ -0,0 +1,56 @@
+import { useState } from "react"
+
+export interface IValidate {
+    [key: string]: {
+        required?: boolean;
+        validate: ( value: any ) => boolean;
+        messageError: string;
+    }
+}
+
+export interface IError {
+    [key: string]: string;
+}
+
+
+export function useForm<Type> ( initialState: Type | any, validations: IValidate ) {
+
+    const [values, setValues] = useState<Type>(initialState)
+    const [errors, setErrors] = useState<IError>({})
+    
+    const handlerChange = (e: any) => {
+        const { name, value } = e.target as { name: string, value: string };
+        const newValues = {
+            ...values,
+            [name]: value
+        }
+        
+        const newErrors = Object.keys(validations).reduce((errors, v) => {
+            const { required, validate, messageError } = validations[v];
+            if( required && !newValues[v] ) {
+                return {
+                    ...errors,
+                    [v]: "This field is required"
+                }
+            }
+            if( !validate( newValues[v] ) ) {
+                return {
+                    ...errors,
+                    [v]: messageError
+                }
+            }
+            return errors;
+        }, {})
+
+        setValues(newValues);
+        setErrors(newErrors);
+    }
+
+    return {
+        handlerChange,
+        validations,
+        values,
+        errors
+    }
+
+}
