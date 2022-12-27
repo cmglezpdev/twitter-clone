68c431eef087002a2e6db9d5270849f6ce147e3d
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-27 15:00:14 -0500

    feat: add auth layout to login and register page

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -5,6 +5,7 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai'
 
 import { useForm } from '../../hooks';
 import { validations as vdts } from '../../services';
+import { AuthLayout } from '../../layouts';
 
 interface FormData {
     name?: string;
@@ -46,102 +47,107 @@ const RegisterPage = () => {
     }
 
     return (
-        <div className='w-1/3 mx-auto mt-10 flex flex-col items-center'>
-            <BsTwitter 
-                className='text-twitter-blue text-6xl' 
-            />
-            <h1 className='text-3xl font-bold mt-6'>Join Twitter today</h1>
-            <form
-                className='w-full mt-5 flex flex-col'
-            >
-                <div className='w-full'>
-                    <input 
-                        className='w-full border-2 border-gray-300 rounded-md p-3 mt-2 text-xl outline-none focus:border-twitter-blue'
-                        type='text' 
-                        name='name' 
-                        placeholder='Name'
-                        onChange={handlerChange}
-                        style={ errors.name ? { borderColor: 'red' } : {} }
-                    />
-                    <span
-                        className='text-red-600 text-sm'
-                        style={{ display: errors.name ? 'block' : 'none' }}
-                    >{ errors.name }</span>
-                </div>
-
-                <div className='w-full'>
-                    <input 
-                        className='w-full border-2 border-gray-300 rounded-md p-3 mt-2 text-xl outline-none focus:border-twitter-blue'
-                        type='text' 
-                        name='email' 
-                        placeholder='Email'
-                        onChange={handlerChange}
-                        style={ errors.email ? { borderColor: 'red' } : {} }
-                    />
-                    <span
-                        className='text-red-600 text-sm'
-                        style={{ display: errors.email ? 'block' : 'none' }}
-                    >{ errors.email }</span>
-                </div>
-
-                <div className='w-full relative'>
-                    <input 
-                        className='w-full border-2 border-gray-300 rounded-md p-3 mt-2 text-xl outline-none focus:border-twitter-blue'
-                        type={ showPassword ? 'text' : 'password' }
-                        name='password' 
-                        placeholder='Password'
-                        onChange={handlerChange}
-                        style={ errors.password ? { borderColor: 'red' } : {} }
-                    />
-                    <AiOutlineEye
-                        style={{ 
-                            display: showPassword ? 'none' : 'block',
-                            color: errors.password ? 'red' : ''
-                        }}
-                        className='absolute right-2 top-7 text-xl cursor-pointer'
-                        onClick={() => setShowPassword(!showPassword)}
-                    />
-                    <AiOutlineEyeInvisible 
-                        style={{ 
-                            display: showPassword ? 'block' : 'none',
-                            color: errors.password ? 'red' : ''
-                        }}
-                        className='absolute right-2 top-7 text-xl cursor-pointer'
-                        onClick={() => setShowPassword(!showPassword)} 
-                    />
-                    
-
-                    <span
-                        className='text-red-600 text-sm'
-                        style={{ display: errors.password ? 'block' : 'none' }}
-                    >{ errors.password }</span>
-                </div>
-
-                <div className='mt-2'>
-                    <h3 className='font-bold text-lg'>Date of birth</h3>
-                    <input 
-                        className='w-full border-4 border-gray-300 rounded-md p-3 my-2 text-xl outline-none focus:border-twitter-blue'
-                        type='date' 
-                        name='brithday'
-                        onChange={handlerChange}
-                        style={ errors.brithday ? { borderColor: 'red' } : {} }
-                    />
-                    <span
-                        className='text-red-600 text-sm'
-                        style={{ display: errors.brithday ? 'block' : 'none' }}
-                    >{ errors.brithday }</span>
-                </div>
-
-                <button
-                    className='bg-twitter-blue disabled:bg-blue-300 hover:bg-blue-500 transition-colors py-4 px-10 font-bold text-white rounded-full w-full mt-5 text-xl outline-none'
-                    onClick={onSubmit}
-                    disabled={ Object.keys(errors).length > 0 || Object.keys(values).length === 0 }
+        <AuthLayout
+            title='Register / Twitter'
+            pageDescription='Join Twitter today'
+        >
+            <div className='w-1/3 mx-auto mt-10 flex flex-col items-center'>
+                <BsTwitter 
+                    className='text-twitter-blue text-6xl' 
+                />
+                <h1 className='text-3xl font-bold mt-6'>Join Twitter today</h1>
+                <form
+                    className='w-full mt-5 flex flex-col'
                 >
-                    Sign up
-                </button>
-            </form>
+                    <div className='w-full'>
+                        <input 
+                            className='w-full border-2 border-gray-300 rounded-md p-3 mt-2 text-xl outline-none focus:border-twitter-blue'
+                            type='text' 
+                            name='name' 
+                            placeholder='Name'
+                            onChange={handlerChange}
+                            style={ errors.name ? { borderColor: 'red' } : {} }
+                        />
+                        <span
+                            className='text-red-600 text-sm'
+                            style={{ display: errors.name ? 'block' : 'none' }}
+                        >{ errors.name }</span>
+                    </div>
 
-        </div>
+                    <div className='w-full'>
+                        <input 
+                            className='w-full border-2 border-gray-300 rounded-md p-3 mt-2 text-xl outline-none focus:border-twitter-blue'
+                            type='text' 
+                            name='email' 
+                            placeholder='Email'
+                            onChange={handlerChange}
+                            style={ errors.email ? { borderColor: 'red' } : {} }
+                        />
+                        <span
+                            className='text-red-600 text-sm'
+                            style={{ display: errors.email ? 'block' : 'none' }}
+                        >{ errors.email }</span>
+                    </div>
+
+                    <div className='w-full relative'>
+                        <input 
+                            className='w-full border-2 border-gray-300 rounded-md p-3 mt-2 text-xl outline-none focus:border-twitter-blue'
+                            type={ showPassword ? 'text' : 'password' }
+                            name='password' 
+                            placeholder='Password'
+                            onChange={handlerChange}
+                            style={ errors.password ? { borderColor: 'red' } : {} }
+                        />
+                        <AiOutlineEye
+                            style={{ 
+                                display: showPassword ? 'none' : 'block',
+                                color: errors.password ? 'red' : ''
+                            }}
+                            className='absolute right-2 top-7 text-xl cursor-pointer'
+                            onClick={() => setShowPassword(!showPassword)}
+                        />
+                        <AiOutlineEyeInvisible 
+                            style={{ 
+                                display: showPassword ? 'block' : 'none',
+                                color: errors.password ? 'red' : ''
+                            }}
+                            className='absolute right-2 top-7 text-xl cursor-pointer'
+                            onClick={() => setShowPassword(!showPassword)} 
+                        />
+                        
+
+                        <span
+                            className='text-red-600 text-sm'
+                            style={{ display: errors.password ? 'block' : 'none' }}
+                        >{ errors.password }</span>
+                    </div>
+
+                    <div className='mt-2'>
+                        <h3 className='font-bold text-lg'>Date of birth</h3>
+                        <input 
+                            className='w-full border-4 border-gray-300 rounded-md p-3 my-2 text-xl outline-none focus:border-twitter-blue'
+                            type='date' 
+                            name='brithday'
+                            onChange={handlerChange}
+                            style={ errors.brithday ? { borderColor: 'red' } : {} }
+                        />
+                        <span
+                            className='text-red-600 text-sm'
+                            style={{ display: errors.brithday ? 'block' : 'none' }}
+                        >{ errors.brithday }</span>
+                    </div>
+
+                    <button
+                        className='bg-twitter-blue disabled:bg-blue-300 hover:bg-blue-500 transition-colors py-4 px-10 font-bold text-white rounded-full w-full mt-5 text-xl outline-none'
+                        onClick={onSubmit}
+                        disabled={ Object.keys(errors).length > 0 || Object.keys(values).length === 0 }
+                    >
+                        Sign up
+                    </button>
+                </form>
+
+            </div>
+        </AuthLayout>
     )
 }
 
