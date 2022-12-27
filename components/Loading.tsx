058b769a7bea6b2c10dd8d27020b7bfa1a61b3dd
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-27 10:34:52 -0500

    feat: add Loading page

---
+++ components/Loading.tsx
@@ -0,0 +1,11 @@
+import { BsTwitter } from 'react-icons/bs'
+
+export const Loading = () => {
+    return (
+        <div className='w-screen h-screen bg-white flex justify-center items-center'>
+            <BsTwitter
+                className='text-[100px] text-twitter-blue'
+            />
+        </div>
+    )
+}
