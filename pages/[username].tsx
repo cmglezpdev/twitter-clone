f26041a2cfe21033220836ae8ac9196f7110542b
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-28 14:28:56 -0500

    feat: add static generation with the profile pages

--- pages/[username].tsx
+++ pages/[username].tsx
@@ -1,13 +1,27 @@
+import { GetStaticProps, GetStaticPaths, NextPage } from 'next'
 import Image from 'next/image';
+
 import { AppLayout } from '../layouts';
+import { dbUsers } from '../database';
+import { IUser } from '../interfaces';
 
 import img from '../public/avatar.png'
 
-const ProfilePage = () => {
+interface Props {
+    user: IUser;
+}
+
+
+const ProfilePage:NextPage<Props> = ({ user }) => {
+    
+    console.log({ user });
+    const { name, username, email, bio } = user;
+
+
     return (
         <AppLayout
-            title="Profile / Twitter"
-            pageDescription="Twitter clone"
+            title={`${name} (${username}) / Twitter`}
+            pageDescription={ !bio ? `Profile of ${name} (${username})` : bio }
         >
             <header className='mt-[70px]'>
                 <div className='relative'>
@@ -15,7 +29,8 @@ const ProfilePage = () => {
                         {/* image without banner */}
                     </div>
                     <div className='absolute w-32 h-32 rounded-full p-1 bg-white left-4 -bottom-1'>
-                        <Image src={img} alt='avatar' className='rounded-full w-full h-full' />
+                       {/* Change by images */}
+                       <Image src={img} alt='avatar' className='rounded-full w-full h-full' />
                     </div>
                     <div className='flex w-full justify-end mt-3'>
                         <button className='border-gray-400 hover:bg-gray-200 transition-colors border-2 text-lg font-bold py-2 px-4 rounded-full'>
@@ -27,10 +42,10 @@ const ProfilePage = () => {
 
                 <div className='mt-4'>
                     <div className='flex flex-col text-lg'>
-                        <span className='font-bold text-xl'>Carlos M. González</span>
-                        <span className='text-gray-700'>@cmglezpdev</span>
+                        <span className='font-bold text-xl'>{ name }</span>
+                        <span className='text-gray-700'>@{ username }</span>
                         <p className='mt-3'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi explicabo, sit dolor, minima nemo distinctio exercitationem possimus eaque deserunt ipsum, hic officiis sapiente soluta. Accusamus veritatis quae dicta aliquam aliquid.
+                            { bio }
                         </p>
                     </div>
                 </div>
@@ -42,3 +57,32 @@ const ProfilePage = () => {
 }
 
 export default ProfilePage;
+
+
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+    const users = await dbUsers.getUsers({});
+    console.log(users);
+    const paths = users.map(({ username }) => ({
+        params: { username }
+    }))
+
+    return {
+        paths,
+        fallback: "blocking"
+    }
+}
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+    const { username } = ctx.params as { username: string };
+    const [ user, ...users ] = await dbUsers.getUsers({ username });
+
+    if( !user )
+        return { redirect: { destination: '/', permanent: false } }
+
+    return {
+        props: { user }
+    }
+}
