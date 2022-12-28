0148fceb968c95b64f81c32352bdbebb9ec08c8a
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-27 19:46:06 -0500

    feat: add auth provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 
 import '../styles/globals.css'
+import { AuthProvider } from '../context/auth'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </SessionProvider>
   )
 }
