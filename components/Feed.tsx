657818b396c51375d123dadc67f31a0f9a5bfac8
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-28 16:51:18 -0500

    feat: show tweets in the feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,13 +1,23 @@
+import useSWR from 'swr';
+import { ITweet } from '../interfaces';
 import { Tweet } from "./Tweet"
 
+
 export const Feed = () => {
-    return (
-      <div className="w-full h-screen px-2 mt-[70px]">
-        <Tweet />
-        <Tweet />
-        <Tweet />
-        <Tweet />
-        <Tweet />
-      </div>
-    )
+  
+  const { data, error, isLoading } = useSWR<ITweet[]>('/api/tweets');
+
+  if( isLoading ) return <div>Loading...</div>
+
+  return (
+    <div className="w-full h-screen px-2 mt-[70px]">
+      
+      {
+        data!.map(tweet => (
+          <Tweet key={tweet._id} tweet={tweet} />
+        ))
+      }
+      
+    </div>
+  )
 }
