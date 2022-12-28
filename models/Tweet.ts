3b89bc852ad60a08e11fc3685c423447ad458095
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-28 14:30:25 -0500

    style: objects

--- models/Tweet.ts
+++ models/Tweet.ts
@@ -2,12 +2,12 @@ import mongoose, { Schema, model, Model } from 'mongoose';
 import { ITweet } from '../interfaces';
 
 const tweetSchema = new Schema({
-    text: { type: String, required: true },
-    user: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
-    views: { type: [mongoose.Types.ObjectId], ref: 'User' },
-    likes: { type: [mongoose.Types.ObjectId], ref: 'User' },
-    retweets: { type: [mongoose.Types.ObjectId], ref: 'User' },
-    comments: { type: [mongoose.Types.ObjectId], ref: 'Tweet' },
+    text     : { type: String, required: true },
+    user     : { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
+    views    : { type: [mongoose.Types.ObjectId], ref: 'User' },
+    likes    : { type: [mongoose.Types.ObjectId], ref: 'User' },
+    retweets : { type: [mongoose.Types.ObjectId], ref: 'User' },
+    comments : { type: [mongoose.Types.ObjectId], ref: 'Tweet' },
 }, {
     timestamps: true
 })
