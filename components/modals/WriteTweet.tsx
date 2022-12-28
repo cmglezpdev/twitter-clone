a387abae8ac4cc714af081fd0170e80d63503f86
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-28 17:50:34 -0500

    feat: save tweet when is writed

--- components/modals/WriteTweet.tsx
+++ components/modals/WriteTweet.tsx
@@ -1,10 +1,13 @@
-import { FC, MouseEvent, useRef } from 'react';
+import { FC, MouseEvent, useRef, useState, useContext } from 'react';
 import Image from 'next/image';
 
 import { IoMdClose } from 'react-icons/io'
 import { GoFileMedia } from 'react-icons/go'
 import { BsEmojiSmile } from 'react-icons/bs'
 import { AiOutlineFileGif } from 'react-icons/ai'
+
+import { twitterApi } from '../../api';
+import { AuthContext } from '../../context/auth';
 
 import img from '../../public/avatar.png'
 
@@ -15,19 +18,26 @@ interface Props {
 
 export const WriteTweetModal:FC<Props> = ({ open, closeModal }) => {
 
+    const [tweetContent, setTweetContent] = useState('');
+    const { user } = useContext(AuthContext);
     const bgModal = useRef<HTMLDivElement>(null);
-
     if (!open) return null;
 
     const handleModal = ( e:MouseEvent<HTMLDivElement> ) => {
         if( bgModal.current === e.target ) closeModal()
     }
 
+    const onTwitear = async (e:any) => {
+        e.preventDefault();
+        await twitterApi.post('/tweets/create', { text: tweetContent, userId: user?._id });
+        closeModal();
+    }
+
     return (
         <div ref={bgModal} className='fixed inset-0 flex bg-modal z-inf' onClick={(e) => handleModal(e)}>
             <div className='flex w-[50%] flex-wrap m-auto bg-white p-3 rounded-lg'>
                 <div className='w-full mb-3'>
-                    <div 
+                    <div
                         className='hover:bg-gray-300 p-2 inline-block transition-colors rounded-full'
                         onClick={closeModal}
                     >
@@ -43,6 +53,7 @@ export const WriteTweetModal:FC<Props> = ({ open, closeModal }) => {
                         rows={5}
                         placeholder="What's happening?" 
                         className='outline-none w-full resize-none text-2xl border-b-2 border-gray-300'
+                        onChange={(e) => setTweetContent(e.target.value)}
                     />
                     <div className='w-full flex justify-between'>
                         <div>
@@ -57,7 +68,10 @@ export const WriteTweetModal:FC<Props> = ({ open, closeModal }) => {
                             </div>
                         </div>
                         
-                        <button className='px-5 py-1 font-bold text-xl bg-blue-500 hover:bg-blue-600 text-white rounded-full transition-colors'>
+                        <button 
+                            className='px-5 py-1 font-bold text-xl bg-blue-500 hover:bg-blue-600 text-white rounded-full transition-colors outline-none'
+                            onClick={onTwitear}
+                        >
                             Tweet
                         </button>
                     </div>
