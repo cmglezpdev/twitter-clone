76c77bc6a2d3fceabb3169c5959c11043c1e2e06
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-28 16:50:23 -0500

    feat: add endpoint to get a tweet

---
+++ pages/api/tweets/[id].ts
@@ -0,0 +1,39 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database';
+import { ITweet } from '../../../interfaces';
+import { Tweet } from '../../../models';
+
+type Data = 
+    | { message: string }
+    | ITweet
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    
+    switch( req.method ) {
+        case 'GET':
+            return getTweet(req, res);
+     
+        default:
+            return res.status(400).json({ message: 'Bad Request' })
+    }
+}
+
+async function getTweet(req: NextApiRequest, res: NextApiResponse<Data>) {
+    
+    const { id } = req.query;
+    console.log(id);
+
+    try {
+        db.connect();
+        const tweet = await Tweet.findById(id).lean();
+        db.disconnect();
+        
+        if( !tweet ) 
+            return res.status(404).json({ message: 'Tweet not found' })
+
+        return res.status(200).json(tweet);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Internal Server Error' })
+    }
+}
