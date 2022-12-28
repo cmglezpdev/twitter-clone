29cc3dd9defada9acaec2070dbfb1bd7243ceeae
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-28 11:52:58 -0500

    fix: add pasword to the user model

--- models/User.ts
+++ models/User.ts
@@ -4,6 +4,7 @@ import { IUser } from '../interfaces';
 const userSchema = new Schema({
     name: { type: String, required: true },
     username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     bibliography: { type: String },
     avatar: { type: String },
