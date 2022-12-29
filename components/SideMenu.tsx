99a7daacf6a3935cf40277af21fe4f0dbde9266f
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-28 20:17:51 -0500

    feat: show basic modal in the side menu

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,18 +1,23 @@
-import { useState } from 'react';
+import { useState, useContext, MouseEvent } from 'react';
 import Link from 'next/link';
+import Image from 'next/image'
 
-import { AiFillHome } from 'react-icons/ai';
+import { AiFillHome, AiOutlineDown } from 'react-icons/ai';
 import { IoIosNotifications } from 'react-icons/io';
 import { CgProfile } from 'react-icons/cg'
 import { FaHashtag } from 'react-icons/fa'
 import { BsTwitter } from 'react-icons/bs'
 
-import { WriteTweetModal } from './modals';
+import { WriteTweetModal, BasicModal } from './modals';
+import { AuthContext } from '../context/auth';
 
+import img from '../public/avatar.png';
 
 export const SideMenu = () => {
 
     const [openTweetModal, setOpenTweetModal] = useState(false);
+    const { user } = useContext(AuthContext);
+    const [openMiniModal, setOpenMiniModal] = useState(false);
 
     return (
         <div className='w-full h-screen p-10 px-5'>
@@ -55,7 +60,40 @@ export const SideMenu = () => {
                 Tweet
             </button>
 
-            <WriteTweetModal 
+            {
+                user &&
+                <div className='flex items-center justify-between mt-7 w-[85%] cursor-pointer hover:bg-gray-200 rounded-full p-2'>
+                    <Link href='/cmglezpdev'>
+                        <Image 
+                            src={img} alt='avatar' 
+                            width={50} height={50}
+                            style={{ minWidth: 50, minHeight: 50 }}
+                            className='rounded-full mr-2' 
+                        />
+                    </Link>
+                    <Link href='/cmglezpdev'>
+                        <span className='font-semibold block'>{ user.name.slice(0, 17).concat('...') }</span>
+                        <span className=''>{user.username}</span>
+                    </Link>
+                    
+                    <AiOutlineDown className='text-lg' onClick={() => setOpenMiniModal(true)} />
+                </div>
+            }
+
+            <BasicModal
+                className='absolute z-10 w-screen h-screen top-0 left-0'
+                open={openMiniModal}
+                closeModal={() => setOpenMiniModal(false)}
+            >
+                <div className='py-5 px-2 border-2 border-gray-100 bg-white rounded-md shadow-md shadow-gray-600 w-64 relative top-[320px] left-36'>
+                    <span 
+                        className='font-bold p-2 hover:bg-gray-100 cursor-pointer block w-full'
+                    >Log out @{user?.username}</span>
+                </div>
+            </BasicModal>
+
+
+            <WriteTweetModal
                 open={openTweetModal}
                 closeModal={() => setOpenTweetModal(false)}
             />
