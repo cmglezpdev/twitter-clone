dec7526631be9da1d3722a70991357b9f315e6db
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-29 12:37:35 -0500

    feat: add interaction with the profile buttons

---
+++ components/profile/Buttons.tsx
@@ -0,0 +1,65 @@
+import { FC, useState, useContext, useEffect } from 'react';
+
+import { IoNotificationsOffOutline, IoNotificationsOutline } from 'react-icons/io5'
+import { AuthContext } from '../../context/auth';
+import { IUser } from '../../interfaces';
+import { twitterApi } from '../../api';
+
+interface Props {
+    user: IUser;
+    openSettingsProfile: () => void;
+}
+
+export const Buttons:FC<Props> = ({ user, openSettingsProfile }) => {
+    
+    const { user: userAuth } = useContext(AuthContext);
+    const [isFollowing, setIsFollowing] = useState(false);
+
+    useEffect(() => {
+        setIsFollowing(user.followers.includes(userAuth?._id || ''));
+    }, [user.followers, userAuth?._id])
+
+    const onFollow =  async (follow: boolean) => {
+        await twitterApi.put(`/users/${user._id}/follow`);
+        setIsFollowing(follow);
+    }
+
+    return (
+        <div className='flex w-full justify-end mt-3 pr-5'>
+            <button
+                className='border-gray-300 hover:bg-gray-200 transition-colors border-2 text-md font-bold py-1 px-4 rounded-full'
+                style={{ display: userAuth?._id === user._id ? 'block' : 'none' }}
+                onClick={openSettingsProfile}
+            >
+                Edit Profile
+            </button>
+    
+            <div style={{ display: userAuth?._id !== user._id ? 'flex' : 'none' }} className=' gap-3'>
+                <div 
+                    className='hover:bg-gray-300 p-2 rounded-full border-2 border-gray-300 transition-colors'
+                    style={{ display: isFollowing ? 'block' : 'none' }}
+                >
+                    <IoNotificationsOutline className='text-xl' />
+                </div>
+
+                <button 
+                    className='group px-4 py-1 rounded-full border-2 border-black bg-black hover:bg-gray-900 min-w-[110px]'
+                    style={{ display: isFollowing ? 'none' : 'block' }}
+                    onClick={() => onFollow(true)}
+                >
+                    <span className='font-bold text-md text-white'>Follow</span>
+                </button>
+
+                <button 
+                    className='group px-4 py-1 rounded-full border-2 border-gray-300 hover:border-red-300 hover:bg-red-200 min-w-[110px]'
+                    style={{ display: isFollowing ? 'block' : 'none' }}
+                    onClick={() => onFollow(false)}
+                >
+                    <span className='block group-hover:hidden font-bold text-md'>Following</span>
+                    <span className='hidden group-hover:block font-bold text-md text-red-600'>Unfollow</span>
+                </button>
+            </div>
+
+        </div>
+    )
+}
