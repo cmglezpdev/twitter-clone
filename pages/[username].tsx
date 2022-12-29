90b3d7ca5ac479ce11ce53cf3af0842da2f74581
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-28 20:40:28 -0500

    feat: show followers

--- pages/[username].tsx
+++ pages/[username].tsx
@@ -15,7 +15,7 @@ interface Props {
 const ProfilePage:NextPage<Props> = ({ user }) => {
     
     console.log({ user });
-    const { name, username, email, bio } = user;
+    const { name, username, bio, followers, following } = user;
 
 
     return (
@@ -39,7 +39,6 @@ const ProfilePage:NextPage<Props> = ({ user }) => {
                     </div>
                 </div>
 
-
                 <div className='mt-4'>
                     <div className='flex flex-col text-lg'>
                         <span className='font-bold text-xl'>{ name }</span>
@@ -47,6 +46,17 @@ const ProfilePage:NextPage<Props> = ({ user }) => {
                         <p className='mt-3'>
                             { bio }
                         </p>
+                    </div>
+                </div>
+
+                <div className='mt-3 flex'>
+                    <div className='flex items-center'>
+                        <span className='font-bold text-md mr-2'>{ following.length }</span>
+                        <span className='text-gray-700'>Following</span>
+                    </div>
+                    <div className='ml-4'>
+                        <span className='font-bold text-md mr-2'>{ followers.length }</span>
+                        <span className='text-gray-700'>Followers</span>
                     </div>
                 </div>
 
