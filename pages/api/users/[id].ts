d1307fd12beea0f72ef34ea252821c6573f997aa
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-28 22:11:33 -0500

    feat: add endpoint to update an user

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { db } from '../../../database';
 import { IUser } from '../../../interfaces';
 import { User } from '../../../models';
+import { validations } from '../../../services';
 
 type Data = 
     | { message: string }
@@ -13,15 +14,16 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
         case 'GET':
             return getUser(req, res);
      
+        case 'PUT':
+            return updateUser(req, res);
+
         default:
             return res.status(400).json({ message: 'Bad Request' })
     }
 }
 
 async function getUser(req: NextApiRequest, res: NextApiResponse<Data>) {
-    
     const { id } = req.query;
-    console.log(id);
 
     try {
         db.connect();
@@ -35,5 +37,40 @@ async function getUser(req: NextApiRequest, res: NextApiResponse<Data>) {
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: 'Internal Server Error' })
+    }
+}
+
+async function updateUser(req: NextApiRequest, res: NextApiResponse<Data>) {
+    const { id } = req.query;
+
+    const { name, username, email, bio, location, website, brith } = req.body;
+    if( !validations.isValidName(name) ) return res.status(400).json({ message: 'Invalid name' });
+    if( !validations.isValidBio(bio) ) return res.status(400).json({ message: 'Invalid bio' });
+    if( !validations.isValidLocation(location) ) return res.status(400).json({ message: 'Invalid location' });
+    if( !validations.isValidWebsite(website) ) return res.status(400).json({ message: 'Invalid website' });
+    if( !validations.isValidBirthday(brith) ) return res.status(400).json({ message: 'Invalid birth' });
+    
+    // if( !validations.isValidUsername(username) ) return res.status(400).json({ message: 'Invalid username' });
+    // if( !validations.isValidEmail(email) ) return res.status(400).json({ message: 'Invalid email' });
+
+    // TODO: que el username solo se pueda cambiar una vez
+    // TODO: que no se actualice el password sin antes verificar la contraseña actual
+
+    const updated = req.body;
+    delete updated.currentPassword;
+    delete updated.newPassword;
+    delete updated.confirmPassword;
+
+    try {
+        db.connect();    
+        const user = await User.findByIdAndUpdate(id, updated, { new: true }).lean();
+        if( !user )
+            return res.status(400).json({ message: 'User no found' });
+
+        db.disconnect();
+        return res.status(200).json(user)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Internal server error' })
     }
 }
