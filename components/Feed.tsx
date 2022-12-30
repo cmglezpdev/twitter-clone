624131a7cf94fe12e9e601d9d3b42aa255369a3f
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-12-29 22:13:54 -0500

    feat: show tweets in the Feed by props endpoint

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,11 +1,15 @@
+import { FC } from 'react';
 import useSWR from 'swr';
 import { ITweet } from '../interfaces';
 import { Tweet } from "./Tweet"
 
+interface Props {
+  endpoint: string;
+}
 
-export const Feed = () => {
+export const Feed:FC<Props> = ({ endpoint }) => {
   
-  const { data, error, isLoading } = useSWR<ITweet[]>('/api/tweets');
+  const { data, error, isLoading } = useSWR<ITweet[]>(endpoint);
 
   if( isLoading ) return <div>Loading...</div>
 
